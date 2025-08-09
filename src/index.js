045919baba0f93a620b7fdc3a2661a81e05fd446
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-09 21:59:37 +0530

    Write a GET request i.e. /ping request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import express from "express";
 // Create a app object
 const app = express();
 
+app.get("/ping", (req, res) => {
+  return res.json({
+    message: "Pong",
+  });
+});
+
 // Define a PORT and attach it to the express app
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
