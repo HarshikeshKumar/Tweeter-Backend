2ef8399da059f54e2a8df85d08bd976e77fd9ed1
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-11 02:07:58 +0530

    now Added business logics for tweet created and Implemented whole MVC (controllers call service layer and service layer call repository layer and the repository layer call DB)

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -1,3 +1,5 @@
+import { createTweet as createTweetService } from "../services/tweetService.js";
+
 export const getTweet = (req, res) => {
   return res.json({
     message: "Welcome to tweet routes",
@@ -11,8 +13,27 @@ export const getTweetById = (req, res) => {
   });
 };
 
-export const createTweet = (req, res) => {
-  return res.json({
-    message: "Create tweet",
-  });
+export const createTweet = async (req, res) => {
+  try {
+    const response = await createTweetService({
+      body: req.body.body,
+    });
+    return res.status(201).json({
+      success: true,
+      data: response,
+      message: "Tweet created Successfully",
+    });
+  } catch (error) {
+    if (error.status) {
+      return res.status(error.status).json({
+        message: error.message,
+        success: false,
+      });
+    }
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
 };
--- src/services/tweetService.js
+++ src/services/tweetService.js
@@ -1,3 +1,22 @@
+import { Filter } from "bad-words";
+import { createTweet as createTweetRepository } from "../repositories/tweetRepository.js";
+
 export const createTweet = async ({ body }) => {
   // try to find blocked words in the tweet body and if any exists don't create the tweet and return an error message
+
+  const filter = new Filter();
+
+  if (filter.isProfane(body)) {
+    console.log(body);
+    console.log(filter.clean(body));
+    // throw new Error("Tweet contains blocked words");
+    throw {
+      message: "Tweet contains blocked words",
+      status: 400,
+    };
+  }
+
+  // Agar blocked words nhi hai toh repository layer ko call krr do
+  const tweet = await createTweetRepository({ body });
+  return tweet;
 };
