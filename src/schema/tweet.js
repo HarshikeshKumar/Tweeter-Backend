0363b876fe54fcf0847cc0357c32d2c3096f9e7c
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-11 01:15:03 +0530

    Created tweetSchema

---
+++ src/schema/tweet.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 280,
+  },
+});
+
+const Tweet = mongoose.model("Tweet", tweetSchema); //Tweet collection
+export default Tweet;
