1670285a9a9587b8579cdfa59703d2756eb4f4ba
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-10 21:35:48 +0530

    Now I have completed /api/comments routes

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import tweetRouter from "./tweet.js";
+import commentRouter from "./comment.js";
 
 const router = express.Router();
 
 router.use("/tweets", tweetRouter); // If the remaining req url starts with /tweets, use tweetRouter
+
+router.use("/comments", commentRouter);
 
 export default router;
