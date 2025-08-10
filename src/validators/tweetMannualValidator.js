175f5a08f04bc5205d73a90e9edfe479b9d98463
HarshikeshKumar <harshikeshkoili214@gmail.com>
2025-08-10 23:35:20 +0530

    Added More mannual validators on /api/v1/tweets POST routes

--- src/validators/tweetMannualValidator.js
+++ src/validators/tweetMannualValidator.js
@@ -1,7 +1,13 @@
 export const createMannualValidator = (req, res, next) => {
   if (!req.body.tweet) {
     return res.status(400).json({
-      error: "Tweet required",
+      error: "Tweet is required",
+    });
+  }
+
+  if (req.body.tweet.length > 280) {
+    return res.status(400).json({
+      error: "Tweet must be 280 character or less",
     });
   }
   next();
